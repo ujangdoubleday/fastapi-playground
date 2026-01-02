b4edcee43596d3562fa7784bcd04c09a40bc846b
ujangdoubleday <domba88galing@gmail.com>
2026-01-02 10:46:51 +0700

    feat(api): add websocket endpoint

---
+++ app/api/v4/endpoints/ws.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+
+router = APIRouter()
+
+@router.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await websocket.send_text(f"Message text was: {data}")
+    except WebSocketDisconnect:
+        # Client disconnected
+        pass
